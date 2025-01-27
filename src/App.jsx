6d62ad7ceac359db4fcff7f99f7f59fe1dc05b89
Idah Abubakar Isah <idahabubakar49@gmail.com>
2025-01-27 18:37:10 +0100

    routing error

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import LandingPage from "./pages/LandingPage";
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route>
-    <Route path="/sign-up" element={<SignUp/>}/>
+    <Route path="/" element={<SignUp/>}/>
     <Route path="/login" element={<Login/>}/>
     <Route path="/landing" element={<LandingPage/>}/>
 
