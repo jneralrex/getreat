e3ca9683e8896d7e799b8173793f0b76b22cbb63
Idah Abubakar Isah <idahabubakar49@gmail.com>
2025-01-28 22:59:08 +0100

    About page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,11 +11,11 @@ const Nav = () => {
             <div className="flex justify-evenly gap-2 text-white md:gap-4 items-center text-[7.4px] sm:text-[14px] italic">
                 <div>Become A Provider</div>
                 <div className="relative group inline-block">
-                    <button className=" text-white text-[7.4px] sm:text-[14px]  w-[80px]  md:w-[150px] font-medium border-none rounded cursor-pointer italic">
+                    <button className=" text-white text-[7.4px] sm:text-[14px] sm:w-[80px]  md:w-[150px] font-medium border-none rounded cursor-pointer italic">
                         About us
                     </button>
                     <div className="absolute hidden shadow-lg z-10 group-hover:block">
-                        <div className=" rounded-lg w-[80px] md:w-[150px] bg-black/90 p-4 flex flex-col gap-2 text-white text-center">
+                        <div className="mr-10 rounded-lg  md:w-[150px] bg-black/90 p-4 flex flex-col gap-2 text-white text-center">
                             <Link to="/about" className="block text-[10px] md:text-[14px] hover:text-[#c3671a]">
                                 About us
                             </Link>
